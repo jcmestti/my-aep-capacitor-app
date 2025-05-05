1c1446453cb424242d87ef1006036cc79da18c54
Caue Bruno De Almeida <caue.almeida@telefonica.com>
2025-05-05 18:20:09 -0300

    style: Fixed indentation

--- ios/Sources/AdobeAep/AdobeAepPlugin.swift
+++ ios/Sources/AdobeAep/AdobeAepPlugin.swift
@@ -4,25 +4,25 @@ import Capacitor
 public class AdobeAepPlugin: CAPPlugin {
     private let implementation = AdobeAep()
 
-        @objc func configure(_ call: CAPPluginCall) {
-            guard let appId = call.getString("appId"), !appId.isEmpty else {
-                call.reject("appId is required")
-                return
-            }
-
-            implementation.configure(appId: appId) {
-                call.resolve(["success": true])
-            }
+    @objc func configure(_ call: CAPPluginCall) {
+        guard let appId = call.getString("appId"), !appId.isEmpty else {
+            call.reject("appId is required")
+            return
         }
 
-        @objc func trackEvent(_ call: CAPPluginCall) {
-            guard let xdm = call.getObject("xdm") as? [String: Any] else {
-                call.reject("xdm must be an object")
-                return
-            }
-
-            implementation.trackEvent(xdm: xdm) {
-                call.resolve(["success": true])
-            }
+        implementation.configure(appId: appId) {
+            call.resolve(["success": true])
         }
+    }
+
+    @objc func trackEvent(_ call: CAPPluginCall) {
+        guard let xdm = call.getObject("xdm") as? [String: Any] else {
+            call.reject("xdm must be an object")
+            return
+        }
+
+        implementation.trackEvent(xdm: xdm) {
+            call.resolve(["success": true])
+        }
+    }
 }
